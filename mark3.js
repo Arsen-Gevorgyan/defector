1d2886ec9ee0bcfb498649992e47f27e4b9a3d2f
Arsen Gevorgyan <arsengevorgyan304@gmail.com>
2026-09-06 09:20:26 +0400

    FIx mark2

--- mark3.js
+++ mark3.js
@@ -1,22 +1,27 @@
 export default function bot({ history, memory }) {
     let move;
-    if (history.length < 3) {
-        move = Math.random() < 0.5 ?  "C" : "D";
-    }
-    const recent = history.slice(-5);
 
-    let def = 0;
-    for (const round of recent) {
-        if (round.opponent === "D") {
-            ++def;
+    if (history.length < 3) {
+        move = Math.random() < 0.5 ? "C" : "D";
+    } 
+    else {
+        const recent = history.slice(-5);
+
+        let def = 0;
+
+        for (const round of recent) {
+            if (round.opponent === "D") {
+                ++def;
+            }
+        }
+
+        if (def >= 3) {
+            move = "D";
+        }
+        else {
+            move = history.at(-1).opponent === "D" ? "D" : "C";
         }
     }
 
-    if (def >= 3) {
-        move = "D";
-    }
-    else {
-        move = history.at(-1).opponent === "D" ? "D" : "C";
-    }
     return [move, memory];
 }
